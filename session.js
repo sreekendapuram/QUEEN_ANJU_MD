c8e2e2037c401f42982a43421c5df64d00b31e54
sreekendapuram <yjobin3@gmail.com>
2024-11-16 21:11:45 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=6E0EHQ6a#7hBBU3UaNfp6aEbNF2GNeQFNdolvlbg3weVeXSchvrU",
 };
